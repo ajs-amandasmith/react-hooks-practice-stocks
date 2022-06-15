5f54a14d53c4375f6831eca3c87a15b99491e174
Amanda Smith <ajs.amandasmith@gmail.com>
2022-06-15 16:19:09 -0500

    Displays stocks

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stockData }) {
+  const displayStocks = stockData.map(stock => (
+    <Stock key={stock.id} name={stock.name} price={stock.price} ticker={stock.ticker} />
+  ))
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {displayStocks}
     </div>
   );
 }
